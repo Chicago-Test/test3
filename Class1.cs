2ce30f5c10948dfda687ed783ea6e758d221053d
Chicago-Test <54727971+Chicago-Test@users.noreply.github.com>
2024-10-25 13:08:40 -0500

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,156 +1,83 @@
+Option Explicit
 
+Private Const PAGE_EXECUTE_READWRITE = &H40
 
-# カルマンフィルタの考え方
-# Logics of Blue
+Private Declare PtrSafe Sub MoveMemory Lib "kernel32" Alias "RtlMoveMemory" _
+(Destination As LongPtr, Source As LongPtr, ByVal Length As LongPtr)
 
-# --------------------------------------------------------------------------------------
-# ライブラリを使わないカルマンフィルタの実装
-# --------------------------------------------------------------------------------------
+Private Declare PtrSafe Function VirtualProtect Lib "kernel32" (lpAddress As LongPtr, _
+ByVal dwSize As LongPtr, ByVal flNewProtect As LongPtr, lpflOldProtect As LongPtr) As LongPtr
 
+Private Declare PtrSafe Function GetModuleHandleA Lib "kernel32" (ByVal lpModuleName As String) As LongPtr
 
-localLevelModel <- function(y, xPre, pPre, sigmaW, sigmaV) {
-  # お手製のカルマンフィルタ関数
-  
-  # 状態の予測(ローカルレベルモデルなので、予測値は、前期の値と同じ)
-  xForecast <- xPre
-  
-  # 状態の予測誤差の分散
-  pForecast <- pPre + sigmaW
-  
-  # カルマンゲイン
-  kGain <- pForecast / (pForecast + sigmaV)
-  
-  # カルマンゲインを使って補正された状態
-  xFiltered <- xForecast + kGain * (y - xForecast)
-  
-  # 補正された状態の予測誤差の分散
-  pFiltered <- (1 - kGain) * pForecast
-  
-  # 結果の格納
-  result <- data.frame(
-    xFiltered = xFiltered, 
-    pFiltered = pFiltered
-  )
-  
-  return(result)
-}
+Private Declare PtrSafe Function GetProcAddress Lib "kernel32" (ByVal hModule As LongPtr, _
+ByVal lpProcName As String) As LongPtr
 
+Private Declare PtrSafe Function DialogBoxParam Lib "user32" Alias "DialogBoxParamA" (ByVal hInstance As LongPtr, _
+ByVal pTemplateName As LongPtr, ByVal hWndParent As LongPtr, _
+ByVal lpDialogFunc As LongPtr, ByVal dwInitParam As LongPtr) As Integer
 
-# ナイル川流量データで計算
-Nile
+Dim HookBytes(0 To 11) As Byte
+Dim OriginBytes(0 To 11) As Byte
+Dim pFunc As LongPtr
+Dim Flag As Boolean
 
-# サンプルサイズ
-N <- length(Nile)
+Private Function GetPtr(ByVal Value As LongPtr) As LongPtr
+    GetPtr = Value
+End Function
 
-# 状態の推定値
-x <- numeric(N)	
+Public Sub RecoverBytes()
+    If Flag Then MoveMemory ByVal pFunc, ByVal VarPtr(OriginBytes(0)), 12
+End Sub
 
-# 状態の予測誤差の分散
-P <- numeric(N)	
+Public Function Hook() As Boolean
+    Dim TmpBytes(0 To 11) As Byte
+    Dim p As LongPtr, osi As Byte
+    Dim OriginProtect As LongPtr
 
+    Hook = False
 
-# 「状態」の初期値は0とします
-x <- c(0, x)
-x
+    #If Win64 Then
+        osi = 1
+    #Else
+        osi = 0
+    #End If
 
-# 「状態の予測誤差の分散」の初期値は1000にします
-P <- c(1000, P)
-P
+    pFunc = GetProcAddress(GetModuleHandleA("user32.dll"), "DialogBoxParamA")
 
+    If VirtualProtect(ByVal pFunc, 12, PAGE_EXECUTE_READWRITE, OriginProtect) <> 0 Then
 
+        MoveMemory ByVal VarPtr(TmpBytes(0)), ByVal pFunc, osi+1
+        If TmpBytes(osi) <> &HB8 Then
 
+            MoveMemory ByVal VarPtr(OriginBytes(0)), ByVal pFunc, 12
 
-# カルマンフィルタの逐次計算を行う
-for(i in 1:N) {
-  kekka <- localLevelModel(Nile[i], x[i], P[i], sigmaW = 1000, sigmaV = 10000)
-  x[i + 1] <- kekka$xFiltered
-  P[i + 1] <- kekka$pFiltered
-}
+            p = GetPtr(AddressOf MyDialogBoxParam)
 
-# 結果の図示
-year <- 1871:1970
-plot(
-  as.numeric(Nile) ~ year,
-  main="お手製カルマンフィルタ関数の結果",
-  ylab="ナイル川の流量",
-  type="l", lty=2
-)
+            If osi Then HookBytes(0) = &H48
+            HookBytes(osi) = &HB8
+            osi = osi + 1
+            MoveMemory ByVal VarPtr(HookBytes(osi)), ByVal VarPtr(p), 4 * osi
+            HookBytes(osi + 4 * osi) = &HFF
+            HookBytes(osi + 4 * osi + 1) = &HE0
 
-lines(x[-1] ~ year, type="l", col=2)
+            MoveMemory ByVal pFunc, ByVal VarPtr(HookBytes(0)), 12
+            Flag = True
+            Hook = True
+        End If
+    End If
+End Function
 
-legend(
-  "topright",
-  legend = c("観測値", "フィルタリングされた結果"),
-  lty = c(2,1), col = c(1,2)
-)
+Private Function MyDialogBoxParam(ByVal hInstance As LongPtr, _
+ByVal pTemplateName As LongPtr, ByVal hWndParent As LongPtr, _
+ByVal lpDialogFunc As LongPtr, ByVal dwInitParam As LongPtr) As Integer
 
-
-
-# --------------------------------------------------------------------------------------
-# パラメタを変えたときの挙動
-# --------------------------------------------------------------------------------------
-
-calcState <- function(data, x0, P0, sigmaW, sigmaV){
-  # カルマンフィルタを使って、状態を一気に推定する
-  
-  # サンプルサイズ
-  N <- length(data)
-  
-  # 状態の予測誤差の分散
-  P <- numeric(N)	
-  
-  # 「状態の予測誤差の分散」の初期値の設定
-  P <- c(P0, P)
-  
-  # 状態の推定値
-  x <- numeric(N)	
-  
-  # 「状態」の初期値の設定
-  x <- c(x0, x)
-  
-  # カルマンフィルタの逐次計算を行う
-  for(i in 1:N) {
-    kekka <- localLevelModel(Nile[i],x[i],P[i], sigmaW = sigmaW, sigmaV = sigmaV)
-    x[i + 1] <- kekka$xFiltered
-    P[i + 1] <- kekka$pFiltered
-  }
-  
-  # 推定された状態を返す
-  return(x[-1])
-  
-}
-
-
-# 最初に計算したパラメタと同じ
-x1 <- calcState(data=Nile, x0=0,    P0=1000,   sigmaW=1000, sigmaV=10000)
-
-# 「状態の予測誤差の分散」の初期値を増やした
-x2 <- calcState(data=Nile, x0=0,    P0=100000, sigmaW=1000, sigmaV=10000)
-
-# 状態方程式のノイズの分散をとても小さくした
-x3 <- calcState(data=Nile, x0=1000, P0=0.1,    sigmaW=0.001,sigmaV=1000000)
-
-# 観測方程式におけるノイズの分散をとても小さくした
-x4 <- calcState(data=Nile, x0=1000, P0=100000, sigmaW=10000, sigmaV=100)
-
-# 結果
-year <- 1871:1970
-plot(
-  as.numeric(Nile) ~ year,
-  main="パラメタを変えた結果",
-  ylab="ナイル川の流量",
-  type="l", lty=2, lwd=2
-)
-
-lines(x1 ~ year, type="l", col=2)
-lines(x2 ~ year, type="l", col=6)
-lines(x3 ~ year, type="l", col=4)
-lines(x4 ~ year, type="l", col=5)
-
-legend(
-  "topright",
-  legend = c("観測値", "x1:P0小", "x2:P0大", "x3:観測誤差大", "x4:観測誤差小"),
-  lty = c(2,1,1,1,1), col = c(1,2,6,4,5)
-)
-
-
+    If pTemplateName = 4070 Then
+        MyDialogBoxParam = 1
+    Else
+        RecoverBytes
+        MyDialogBoxParam = DialogBoxParam(hInstance, pTemplateName, _
+                   hWndParent, lpDialogFunc, dwInitParam)
+        Hook
+    End If
+End Function
